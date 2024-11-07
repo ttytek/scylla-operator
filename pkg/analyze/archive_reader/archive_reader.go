74630e1f9abdef7b313322b1b9196e35ace5a8c6
Tytus Walężak <tw448537@students.mimuw.edu.pl>
2024-11-07 20:19:48 +0100

    Add archive reader scaffold

---
+++ pkg/analyze/archive_reader/archive_reader.go
@@ -0,0 +1,22 @@
+package archive_reader
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	admissionv1 "k8s.io/api/admission/v1"
+	v1 "k8s.io/api/admission/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/klog/v2"
+)
+
+func Start(archivePath string){
+	klog.Infof("Archive reader %s", archivePath)
+	
+}
